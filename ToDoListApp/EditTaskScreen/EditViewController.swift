88fa04956810b6f879eee6b38125863e70ebbafb
Tushar86 <tushverma86@gmail.com>
2024-03-11 21:39:36 +0100

    Initial Commit

---
+++ ToDoListApp/EditTaskScreen/EditViewController.swift
@@ -0,0 +1,97 @@
+//
+//  EditViewController.swift
+//  ToDoListApp
+//
+//  Created by Tushar  Verma on 11.03.24.
+//
+
+import UIKit
+
+class EditViewController: UIViewController, UITextViewDelegate {
+
+    var tittleValue : String?
+    var indexvalue : Int?
+    
+    let tittleTextView = UITextView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(backToParentView))
+        navigationItem.rightBarButtonItem?.isEnabled = false
+        
+        // Create a UILabel
+        let taskLabel = UILabel()
+        // Set label properties
+        taskLabel.text = "Tittle"
+        taskLabel.textColor = UIColor.black
+        taskLabel.font = UIFont.systemFont(ofSize: 18)
+        taskLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add label to the view
+        view.addSubview(taskLabel)
+        
+        // Configure constraints
+        NSLayoutConstraint.activate([
+            taskLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            taskLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            taskLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            taskLabel.heightAnchor.constraint(equalToConstant: 50)
+            
+        ])
+        
+        // Set textView properties
+        tittleTextView.textColor = UIColor.black
+        tittleTextView.font = UIFont.systemFont(ofSize: 18)
+        tittleTextView.isScrollEnabled = false // Disable scrolling
+        tittleTextView.delegate = self
+        tittleTextView.layer.borderColor = UIColor.black.cgColor
+        tittleTextView.layer.borderWidth = 0.5
+        
+        if let tittleValue = tittleValue {
+            tittleTextView.text = tittleValue
+        }
+        
+        // Ensure that autoresizing mask is set to false for using auto layout
+        tittleTextView.translatesAutoresizingMaskIntoConstraints = false
+        // Add textView to the view
+        view.addSubview(tittleTextView)
+        
+        // Configure constraints
+        NSLayoutConstraint.activate([
+            tittleTextView.topAnchor.constraint(equalTo: taskLabel.bottomAnchor),
+            tittleTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            tittleTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            tittleTextView.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+    }
+    
+    @objc func backToParentView(){
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @objc func textViewDidChange(_ textView: UITextView) {
+        if(textView.text.isEmpty || textView.text == tittleValue){
+            navigationItem.rightBarButtonItem?.isEnabled = false
+        }
+        else{
+            navigationItem.rightBarButtonItem?.isEnabled = true
+        }
+        adjustTextViewHeight(textView)
+    }
+
+    func adjustTextViewHeight(_ textView: UITextView) {
+        let size = textView.sizeThatFits(CGSize(width: textView.frame.size.width, height: CGFloat.greatestFiniteMagnitude))
+        textView.constraints.forEach { (constraint) in
+            if constraint.firstAttribute == .height {
+                constraint.constant = size.height
+            }
+        }
+        view.layoutIfNeeded()
+    }
+
+}
---
+++ ToDoListApp/ToDoListMainScreen/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Tushar  Verma on 11.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var toDoListTable: UITableView!
+    
+    var dataArray = ["ABC", "DEF", "GHI", "JKL"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        toDoListTable.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: UIBarButtonItem.Style.plain, target: self, action: #selector(addTask))
+    }
+
+    @objc func addTask(){
+        self.navigateToEditScreen(taskTittleValue: nil, tittleIndexValue: nil)
+    }
+    
+    func navigateToEditScreen(taskTittleValue: String?, tittleIndexValue: Int?){
+        lazy var editVC = EditViewController()
+        editVC.tittleValue = taskTittleValue
+        editVC.indexvalue = tittleIndexValue
+        navigationController?.pushViewController(editVC, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = dataArray[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.navigateToEditScreen(taskTittleValue: dataArray[indexPath.row], tittleIndexValue: indexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, sourceView, completion) in
+            self.dataArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            completion(true)
+        }
+        let addSubtaskAction = UIContextualAction(style: .normal, title: "Add Subtask") { (action, sourceView, completion) in
+            self.navigateToEditScreen(taskTittleValue: nil, tittleIndexValue: nil)
+            completion(true)
+            
+        }
+        let swipeConfiguration = UISwipeActionsConfiguration(actions: [deleteAction, addSubtaskAction])
+        return swipeConfiguration
+    }
+}
+
